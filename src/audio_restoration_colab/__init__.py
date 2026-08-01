2170a44e1867b63872ed6839a35fc0447c3b3293
egor125552 <sitkosergej55@gmail.com>
2026-08-01 18:30:21 +0300

    Apply native RoFormer settings in separator runtime

--- src/audio_restoration_colab/__init__.py
+++ src/audio_restoration_colab/__init__.py
@@ -2,7 +2,12 @@
 
 __version__ = "0.1.0"
 
+from .runtime_patches import (
+    apply_audio_separator_quality_patches as _apply_audio_separator_quality_patches,
+)
 from .top_models import apply_top_model_catalog as _apply_top_model_catalog
 
 _apply_top_model_catalog()
+_apply_audio_separator_quality_patches()
 del _apply_top_model_catalog
+del _apply_audio_separator_quality_patches
