ee4c07443c404748ede1df251ca30ce6200cd1ea
egor125552 <sitkosergej55@gmail.com>
2026-08-10 22:52:49 +0300

    Enable studio ensemble progress patch

--- src/audio_restoration_colab/__init__.py
+++ src/audio_restoration_colab/__init__.py
@@ -2,6 +2,9 @@
 
 __version__ = "0.1.0"
 
+from .progress_patches import (
+    apply_studio_progress_patch as _apply_studio_progress_patch,
+)
 from .runtime_patches import (
     apply_audio_separator_quality_patches as _apply_audio_separator_quality_patches,
 )
@@ -9,5 +12,7 @@ from .top_models import apply_top_model_catalog as _apply_top_model_catalog
 
 _apply_top_model_catalog()
 _apply_audio_separator_quality_patches()
+_apply_studio_progress_patch()
 del _apply_top_model_catalog
 del _apply_audio_separator_quality_patches
+del _apply_studio_progress_patch
