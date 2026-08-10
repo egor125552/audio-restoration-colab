ffd611a18d25d65d0b963d7ab62300be0deb1424
egor125552 <sitkosergej55@gmail.com>
2026-08-10 22:36:19 +0300

    Define studio instrumental ensemble

---
+++ src/audio_restoration_colab/studio_instrumental.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+STUDIO_INSTRUMENTAL_PRESET = "instrumental_studio"
+STUDIO_INSTRUMENTAL_ALGORITHM = "median_fft"
+STUDIO_INSTRUMENTAL_MODELS = (
+    "mel_band_roformer_instrumental_fv7z_gabox.ckpt",
+    "bs_roformer_instrumental_resurrection_unwa.ckpt",
+    "mel_band_roformer_instrumental_becruily.ckpt",
+)
